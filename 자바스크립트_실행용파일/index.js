5cec2269ed739ad24a36d451fff4c6fc74598e40
Juseong-Yu <aaa4591@naver.com>
2023-07-12 17:07:13 +0900

    year

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -1,12 +1,7 @@
 const { count } = require('console');
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
-let input = fs.readFileSync(path).toString().split('\n');
+let input = fs.readFileSync(path).toString()
 
-let L = parseInt(input[0].split(' ')[0]);
-let P = parseInt(input[0].split(' ')[1]);
-let news = input[1].split(' ');
-for(let i = 0; i < news.length; i++){
-  console.log(news[i] - (L*P));
-}
-console.log(news)
+let L = parseInt(input);
+console.log(L-543)
