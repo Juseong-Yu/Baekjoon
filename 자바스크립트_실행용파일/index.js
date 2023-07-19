8e5fcc7f70d309bb9872744d04880668812df020
Juseong-Yu <aaa4591@naver.com>
2023-07-19 15:30:51 +0900

    오르막 수

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -4,14 +4,15 @@ const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
 let input = fs.readFileSync(path).toString()
 
 const n = parseInt(input);
-if (n === 1){
-  console.log(1)
-}else{
-  let arr = [1, 3];
-  for (let i = 2; i < n; i++){
-    let tiles = ((arr[i-2]*2) + (arr[i-1])) % 10007
-    arr.push(tiles)
+let arr = [1,1,1,1,1,1,1,1,1,1];
+let tmp = []
+let result = 10
+for (let i = 1; i < n; i++){
+  for (let j = 0; j < 10; j++){
+    tmp.push(arr.slice(0,j+1).reduce((acc,cur) => acc+cur)% 10007)
   }
-  let result = arr[arr.length - 1];
-  console.log(result)
+  result = tmp.reduce((acc,cur) => acc+cur)
+  arr = tmp.slice()
+  tmp = []
 }
+console.log(result % 10007)
