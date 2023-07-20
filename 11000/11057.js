3d3140fc055e46d98f095cfc22a94b1aa4aab04d
Juseong-Yu <aaa4591@naver.com>
2023-07-20 20:40:09 +0900

    별 그리기

--- 11000/11057.js
+++ 11000/11057.js
@@ -1,1 +1,18 @@
-1000
+const { count } = require('console');
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
+let input = fs.readFileSync(path).toString()
+
+const n = parseInt(input);
+let arr = [1,1,1,1,1,1,1,1,1,1];
+let tmp = []
+let result = 10
+for (let i = 1; i < n; i++){
+  for (let j = 0; j < 10; j++){
+    tmp.push(arr.slice(0,j+1).reduce((acc,cur) => acc+cur)% 10007)
+  }
+  result = tmp.reduce((acc,cur) => acc+cur)
+  arr = tmp.slice()
+  tmp = []
+}
+console.log(result % 10007)
--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -2,47 +2,10 @@ const { count } = require('console');
 const fs = require('fs');
 const { parse } = require('path');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
-let input = fs.readFileSync(path).toString().split('\n');
+let input = fs.readFileSync(path).toString()
 
-const N = parseInt(input[0])
-let arr = input[1].split(' ').map((ele) => parseInt(ele));
-arr = [...arr.slice(0,3),arr[5],arr[4],arr[3]]
-let result = 0;
-let point = 4;
-let narrow = (N-2)*8+4
-let space = 5 * (N-2)*(N-2) + (N-2)*4;
-let min_1 = Math.min(...arr)
-let min_2 = Math.max(...arr);
-min_2 = min_2 + min_2
-let min_3 = Math.max(...arr);
-min_3 = min_3 + min_3 + min_3 
-if(N === 1){
-  result = arr.reduce((acc,cur)=>acc+cur) - Math.max(...arr)
-}else{
-  for(let i = 1; i < 6; i++){
-  for (let k = 0; k < i; k++){
-    if(i !== k+3){
-      if(min_2 > arr[i]+arr[k]){
-        min_2 = arr[i]+arr[k]
-      }
-    }
-  }
+const N = parseInt(input)
+for (let i = 0; i < N; i++){
+  let result =  ((N-i)*2-1)
+  console.log(result);
 }
-for (let q = 2; q < 6; q++){
-  for (let w = 1; w < q; w++){
-    for (let e = 0; e < w; e++){
-      if(q !== w+3 && q !== e+3 && w !== e+3){
-        if(min_3 > arr[q]+arr[w]+arr[e]){
-          min_3 = arr[q]+arr[w]+arr[e]
-        }
-      }
-    }
-  }
-}
-if (N === 1){
-  result = arr.reduce((acc,cur) => acc+ cur);
-}else{
-  result = min_1*space + min_2 * narrow + min_3 * point
-}
-}
-console.log(result)
