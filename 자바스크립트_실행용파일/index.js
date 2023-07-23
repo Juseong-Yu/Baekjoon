3fa87e59af0f0e68350d0fb9bc36290d87ef1eb7
Juseong-Yu <aaa4591@naver.com>
2023-07-23 16:07:08 +0900

    가장 큰 증가하는 부분 수열

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -7,30 +7,15 @@ let input = fs.readFileSync(path).toString().split('\n');
 const N = parseInt(input[0]);
 const A = input[1].split(' ').map((ele) => parseInt(ele));
 
-function longarr (arr){
-  let dp = [1]
-  for (let i = 1; i < arr.length; i++){
-    let dpmax = 1
+let dp = [A[0]]
+  for (let i = 1; i < A.length; i++){
+    let dpmax = A[i]
     for (let j = 0; j < i; j++){
-      if (arr[j] < arr[i] && dpmax < dp[j]+1){
-          dpmax = dp[j]+1
+      if (A[j] < A[i] && dpmax < dp[j]+A[i]){
+          dpmax = dp[j] + A[i]
       }
     }
     dp.push(dpmax);
   }
   result = Math.max(...dp);
-  return result
-};
-
-let max = 0
-for (let top = 0; top < A.length; top++){
-  let leftarr = A.slice(0,top+1);
-  let rightarr = A.slice(top);
-
-  let tmpresult = longarr(leftarr) + longarr(rightarr.reverse())-1;
-  if (max < tmpresult){
-    max = tmpresult
-  }
-}
-
-console.log(max)
+  console.log(result);
