2c389bd6f5c0bb072e0ec6c2b4fbf29f2829e7fa
Juseong-Yu <aaa4591@naver.com>
2023-07-23 20:46:53 +0900

    연속부분최대곱

--- 2000/2670.py
+++ 2000/2670.py
@@ -11,3 +11,35 @@ for j in range(1,len(arr)):
   else:
     dp.append(arr[j])
 print('%0.3f' % max(dp))
+
+# const { count } = require('console');
+# const fs = require('fs');
+# const { parse } = require('path');
+# const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
+# let input = fs.readFileSync(path).toString().split('\n');
+
+# const N = parseInt(input[0]);
+# let arr = []
+# for (let i = 1; i <= N; i++){
+#   arr.push(parseFloat(input[i]));
+# }
+
+# let dp = [arr[0]];
+
+# for (let j = 1; j < arr.length; j++){
+#   let max = arr[j]
+#   if (dp[j-1] * arr[j] > arr[j]){
+#     dp.push(Math.round(dp[j-1] *arr[j]*1000)/1000);
+#   }else{
+#     dp.push(arr[j])
+#   }
+# }
+
+# let result = Math.max(...dp);
+
+# result = result * 1000
+
+# result = Math.round(result)
+
+# result = result / 1000
+# console.log(result.toFixed(3))
