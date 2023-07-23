f1a36454527f3bb384767a13a39d7662da22d014
Juseong-Yu <aaa4591@naver.com>
2023-07-23 15:35:56 +0900

    가장 긴 상승수열

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -3,14 +3,18 @@ const fs = require('fs');
 const { parse } = require('path');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
 let input = fs.readFileSync(path).toString().split('\n');
-const L = parseInt(input[0]);
-const A = parseInt(input[1]);
-const B = parseInt(input[2]);
-const C = parseInt(input[3]);
-const D = parseInt(input[4]);
 
-const korean = Math.ceil(A / C);
-const math = Math.ceil(B/D);
-
-const doh = Math.max(korean,math);
-console.log(L-doh)
+const N = parseInt(input[0]);
+const A = input[1].split(' ').map((ele) => parseInt(ele));
+let dp = [1]
+for (let i = 1; i < A.length; i++){
+  let dpmax = 1
+  for (let j = 0; j < i; j++){
+    if (A[j] < A[i] && dpmax < dp[j]+1){
+        dpmax = dp[j]+1
+    }
+  }
+  dp.push(dpmax);
+}
+result = Math.max(...dp);
+console.log(result);
