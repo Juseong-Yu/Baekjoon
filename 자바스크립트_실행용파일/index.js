43fbedb952356efb58662ce856e30d84b3ade4ca
Juseong-Yu <aaa4591@naver.com>
2023-08-04 18:47:28 +0900

    N과 M(4)

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
-let input = fs.readFileSync(path).toString().split('\n');
+let input = fs.readFileSync(path).toString().split(' ');
 
 const N = parseInt(input[0]);
-let result = 0
-for(let i = 1; i <= N; i++){
-  let adder = parseInt(input[i]);
-  adder -=1
-  result += adder 
+const M = parseInt(input[1]);
+let arr = [];
+for (let k = 1; k <= N; k++){
+  arr.push([k])
 }
-console.log(result +1)
+for (let i = 1; i < M; i++){
+  let tmparr = []
+  for (let j = 0; j < arr.length; j++){
+    for(let p = arr[j][arr[i].length-1]; p <= N ; p++){
+      let tmp = arr[j].slice()
+      tmp.push(p)
+      tmparr.push(tmp)
+    }
+  }
+  arr = tmparr.slice()
+}
+for(let idx = 0; idx < arr.length; idx++){
+  console.log(...arr[idx])
+}
