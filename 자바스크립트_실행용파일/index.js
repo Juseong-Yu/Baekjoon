475a19df97bdb2576ebf3e73d90d1eaf95110f42
Juseong-Yu <aaa4591@naver.com>
2023-08-15 13:07:42 +0900

    대화아님 인턴

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -2,40 +2,20 @@ const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
 let input = fs.readFileSync(path).toString().split(' ');
 
-const N = parseInt(input[0]);
-const K = parseInt(input[1]);
-let X = N;
-let arr = [X];
-let times = 0;
-let result = 'notmade'
-if (N === K){
+let N = parseInt(input[0]);
+let M = parseInt(input[1]);
+let K = parseInt(input[2]);
 
-}else{
-  while(true){
-    times++
-    let tmparr = [];
-    while(arr.length > 0){
-      X = arr.shift()
-      if(X+1 === K || X-1 === K || 2*X === K){
-        result = 'made';
-        break
-      }else{
-        if (X+1 > 0 && X+1 <= 100000 && !tmparr.includes(X+1)){
-          tmparr.push(X+1)
-        }
-        if (X-1 > 0 && X-1 <= 200000 && !tmparr.includes(X-1)){
-          tmparr.push(X-1)
-        }
-        if (X*2 > 0 && X*2 <= 200000 && !tmparr.includes(X*2)){
-          tmparr.push(X*2)
-        }
-      }
-    }
-    arr = tmparr.slice()
-    if(result === 'made'){
-      break;
-    }
+for (let i = 0; i < K; i++){
+  if (N / 2 > M){
+    N--
+  }else{
+    M--
   }
 }
 
-console.log(times)
+if (N / 2 > M){
+  console.log(M)
+}else{
+  console.log(Math.floor(N/2))
+}
