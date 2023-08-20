7a1a7827241295d343f7dc875698fc328e8b7eb4
Juseong-Yu <aaa4591@naver.com>
2023-08-20 16:43:03 +0900

    다항함수의 적분

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -1,21 +1,46 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
-let input = fs.readFileSync(path).toString().split(' ');
+let input = fs.readFileSync(path).toString()
+let cal = false
+let result = null
 
-let N = parseInt(input[0]);
-let M = parseInt(input[1]);
-let K = parseInt(input[2]);
 
-for (let i = 0; i < K; i++){
-  if (N / 2 > M){
-    N--
-  }else{
-    M--
-  }
+if(input.indexOf('-') === 0){
+  cal = true;
+  input = input.slice(1);
 }
 
-if (N / 2 > M){
-  console.log(M)
+if(input.includes('+')){
+  input = input.split('x');
+  if(input[0] === ''){
+    result = 1
+  }else{
+    result = input[0];
+  }
+}else if(input.includes('-')){
+  input = input.split('x');
+  if(input[0] === ''){
+    result = 1
+  }else{
+    result = input[0];
+  }
 }else{
-  console.log(Math.floor(N/2))
+  if(input.includes('x')){
+    input = input.split('x');
+    if(input[0] === ''){
+      result = 1
+    }else{
+      result = input[0]
+    }
+    
+  }else{
+    result = 0
+  }
+}
+if(result === 0){
+  console.log(0)
+}else if(cal === true){
+  console.log('-'+result)
+}else{
+  console.log(result)
 }
