5bc036dd22c59b6cc374c87c10819f639c999151
Juseong-Yu <aaa4591@naver.com>
2023-09-21 16:37:33 +0900

    n번째 큰 수

--- 자바스크립트_실행용파일/index.js
+++ 자바스크립트_실행용파일/index.js
@@ -2,37 +2,14 @@ const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
 let input = fs.readFileSync(path).toString().split('\n');
 
-const N = parseInt(input[0]);
-let Pi = input[1].split(' ').map((ele)=> {return parseInt(ele)});
-
-if(N === 0){
-  console.log(0);
-}else{
-  let maxstart = 0;
-  let maxend = 0;
+const T = parseInt(input[0]);
+for (let i = 1; i < T+1; i++){
+  let A = input[i].split(' ').map((ele)=> {return parseInt(ele)});
   let max = 0;
-  let rivalstart = 0;
-  let rivalend = 0;
-  let rival = 0;
-  for (let i = 1; i < Pi.length; i++){
-    if(Pi[i-1] < Pi[i]){
-      if(maxend === i-1){
-        maxend = i;
-        max = Pi[maxend] - Pi[maxstart];
-      }else if(rivalend === i-1){
-        rivalend = i;
-        rival = Pi[rivalend] - Pi[rivalstart]
-        if(rival > max){
-          maxstart = rivalstart;
-          maxend = rivalend;
-          max = rival
-        }
-      }
-    }else{
-      rivalstart = i;
-      rivalend = i;
-      rival = 0
-    }
+  for (let n = 0; n < 3; n++){
+    max = Math.max(...A);
+    index = A.indexOf(max);
+    A.splice(index,1);
   }
   console.log(max)
 }
