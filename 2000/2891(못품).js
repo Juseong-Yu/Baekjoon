a18e441f4bbaf0f7fcbc7780c0a56f5f2b08eb1c
Juseong-Yu <aaa4591@naver.com>
2023-10-09 20:11:46 +0900

    연산자 끼워 넣기

---
+++ 2000/2891(못품).js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'text.txt';
+let input = fs.readFileSync(path).toString().trim().split('\n');
+
+let S = input[1].trim().split(' ').map(ele => {return parseInt(ele)});
+let R = input[2].trim().split(' ').map(ele => {return parseInt(ele)});
+
+filterS = S.filter(ele => !R.includes(ele));
+filterR = R.filter(ele => !S.includes(ele));
+
+let bor = [];
+for(let i = 0; i < filterS.length; i++){
+  if(filterR.includes(filterS[i]-1) || filterR.includes(filterS[i]+1)){
+    bor.push(filterS[i])
+    let idx = filterR.indexOf(filterS[i]);
+    filterR.splice(idx,1)
+  }
+}
+console.log(parseInt(filterS.length - bor.length))
