c59d7effc86250b80e2836e6df242f26927b8259
Juseong-Yu <aaa4591@naver.com>
2024-03-08 22:23:39 +0900

    날짜 계산

--- 1000/1476.py
+++ 1000/1476.py
@@ -1,9 +1,13 @@
-E = int(input())
-S = int(input())
-M = int(input())
-
-for i in range(1,7981):
-  if ((i%15 == E & i%28 == S & i%19 == M) | i == 7980):
+E, S, M = map(int,input().split())
+i = 1
+if E == 15:
+  E = 0
+if S == 28:
+  S = 0
+if M == 19:
+  M = 0
+while(True):
+  if((i % 15 == E and i % 28 == S and i % 19 == M )):
     print(i)
     break
-
+  i += 1
