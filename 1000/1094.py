0e1c7fbba67f1d679334f75ee2209488eba4a72a
juseong Yu <juseongyu56@gmail.com>
2025-07-29 09:03:02 +0900

    막대기

---
+++ 1000/1094.py
@@ -0,0 +1,17 @@
+X = int(input())
+sticks = [64]
+result = 1
+while True:
+    if sticks[0] == X:
+        break
+    while sum(sticks) > X:
+        smash = sticks.pop() / 2
+        if sum(sticks) + smash >= X:
+            sticks.append(smash)
+        else:
+            sticks.extend([smash, smash])
+    result = len(sticks)
+    sticks = [sum(sticks)]
+    if sticks[0] == X:
+        break
+print(result)
