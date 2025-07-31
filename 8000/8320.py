74b842bf49021a44948122f24450fe4174b4bb52
juseong Yu <juseongyu56@gmail.com>
2025-07-31 15:49:49 +0900

    직사각형 만드는 방법

---
+++ 8000/8320.py
@@ -0,0 +1,11 @@
+n = int(input())
+result = 0
+for num in range(1, n + 1):
+    for x in range(1, (num + 1) // 2 + 1):
+        if num % x == 0 :
+            result += 1
+            # print(num, x, result)
+            if  num // x < x:
+                # print(num, x, result, 'out')
+                result -= 1
+print(result)
