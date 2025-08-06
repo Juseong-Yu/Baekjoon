988e41bb7cf8b8f0aae296f074dd948916b6a3a8
juseong Yu <juseongyu56@gmail.com>
2025-08-06 16:30:45 +0900

    fds

--- 13000/13172.py
+++ 13000/13172.py
@@ -1,20 +1,13 @@
+X = 10 ** 9 + 7
+def mod_inverse(b, X):
+    return pow(b, X - 2, X)
+
 M = int(input())
-X = 1000000007
 result = 0
-for m in range(M):
-    b, a = map(int, input().split())
-    while True:
-        if b % a == 0:
-            
-    # (result * b) % X = a 
-    for i in range(1, 10000000007):
-        num = (X * i + a) / b
-        try:
-            tmp = num % 3
-        except:
-            continue
-        else:
-            num = int(num)
-            result += num
-            break
+
+for _ in range(M):
+    N, S = map(int, input().split())
+    inv_N = mod_inverse(N, X)
+    result = (result + S * inv_N) % X
+
 print(result)
