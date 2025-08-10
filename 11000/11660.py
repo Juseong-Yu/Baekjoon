fc7558560ce556b065f9883b15a3767a2d49a53c
Juseong-Yu <aaa4591@naver.com>
2025-08-10 15:49:15 +0900

    누적합

---
+++ 11000/11660.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * (N + 1) for _ in range(N + 1)]
+
+for y in range(1, N + 1):
+    for x in range(1, N + 1):
+        dp[y][x] = dp[y][x-1] + dp[y-1][x] - dp[y-1][x-1]+ arr[y-1][x-1]
+for m in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
+    print(result)
