2ab1db5d0996ec8af0fa673d33d8584e374e2133
subillie <aaa4591@naver.com>
2025-08-14 11:38:04 +0900

    스위치 켜고 끄기

--- 1000/1244.py
+++ 1000/1244.py
@@ -1,44 +1,31 @@
-N = int(input())
-switch = list(map(int, input().split()))
-student_n = int(input())
-students = []
-for _ in range(student_n):
-    student = list(map(int, input().split()))
-    students.append(student)
-
+T = int(input())
+switchs = list(map(int, input().split()))
+student_count = int(input())
+students = [list(map(int, input().split())) for _ in range(student_count)]
+switchs = [0] + switchs
 for student in students:
-    print(student)
     gender = student[0]
-    num = student[1]
-    if gender == 1:
-        for idx in range(num - 1, student_n, num):
-            if switch[idx] == 1:
-                switch[idx] = 0
-            else:
-                switch[idx] = 1
-            print(idx, num, student_n)
-    else:
-        idx = 0
+    pick = student[1]
+    if gender  == 1:
+        for idx in range(pick , T + 1, pick):
+            switchs[idx] = 1 - switchs[idx]
+    elif gender == 2:
+        M = 1
         while True:
-            print('hi')
-            if num + idx < student_n and num - idx >= 0 and switch[num + idx] == switch[num - idx]:
-                if idx == 0:
-                    if switch[idx] == 1:
-                        switch[idx] = 0
-                    else:
-                        switch[idx] = 1
-                else:
-                    if switch[num + idx] == 1:
-                        switch[num + idx] = 0
-                    else:
-                        switch[num + idx] = 1
-
-                    if switch[num - idx] == 1:
-                        switch[num - idx] = 0
-                    else:
-                        switch[num - idx] = 1
-            else:
+            if pick - M < 1 or pick + M > T:
                 break
+            if switchs[pick - M] != switchs[pick + M]:
+                break
+            M += 1
+        switchs[pick] = 1 - switchs[pick]
+        for m in range(1, M):
+            switchs[pick + m] =1 - switchs[pick + m]
+            switchs[pick - m] =1 - switchs[pick - m]
 
-    print(switch)
-        
+for idx, switch in enumerate(switchs):
+    if idx == 0:
+        continue
+    elif idx % 20 == 0:
+        print(switch, end='\n')
+    else:
+        print(switch, end=' ')
