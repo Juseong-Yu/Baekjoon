c45c9b5482bf868b1db1c3323fc5026d44729fa4
JuseongYu <aaa4591@naver.com>
2025-08-22 17:23:45 +0900

    f

--- 2000/tempCodeRunnerFile.py
+++ 2000/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-    # for check in checks:
-    #     for idx, ele in enumerate(order):
-    #         if idx != 0  and (ele + 1) in check and  (order[idx - 1] + 1)  in check:
-    #             if check.index(ele + 1) > check.index(order[idx - 1] + 1):
-    #                 return
+    print(dist)
