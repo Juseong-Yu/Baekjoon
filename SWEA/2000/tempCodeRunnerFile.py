84d681dd51edd83f718b5660c377afe35861bd22
hurgayoung <charmant.rachel@gmail.com>
2025-09-09 16:43:23 +0900

    공부 하기 싫은날

--- SWEA/2000/tempCodeRunnerFile.py
+++ SWEA/2000/tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-    if result > max_result :
-                    max_result = result
+    for num in nums:
+                        tmp_sudoku = copy.deepcopy(sudoku)
+                        tmp_sudoku[y][x] = num
+                        return backtrack(tmp_sudoku, n + 1)
