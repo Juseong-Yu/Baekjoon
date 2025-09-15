6617211e6fe14b69b6654de9c995a03bb2d2c11b
Juseong-Yu <aaa4591@naver.com>
2025-09-15 20:42:19 +0900

    창용마을무리의 개수

--- SWEA/7000/7465.py
+++ SWEA/7000/7465.py
@@ -1,24 +1,28 @@
-T = int(input())
+def union(num1, num2):
+    master1 = find(num1)
+    master2 = find(num2)
+
+    if master1 < master2:
+        parents[master2] = master1
+    else:
+        parents[master1] = master2
+
 def find(num):
-        global master
-        if master[num] == num:
-            return master[num]
-        else:
-            master[num] = find(master[num])
-            return master[num]
-        
+    if parents[num] == num:
+        return parents[num]
+    else:
+        parents[num] = find(parents[num])
+        return parents[num]
+    
+T = int(input())
+
 for t in range(1, T + 1):
     N, M = map(int, input().split())
-    master = [i for i in range(N + 1)]
+    parents = [i for i in range(N + 1)]
     for _ in range(M):
         num1, num2 = map(int, input().split())
-        if num1 > num2:
-            small = num2
-            big = num1
-        else:
-            small = num1
-            big = num2
-        master[find(big)] = find(small)
+        union(num1, num2)
     
-    result = len({find(i) for i in range(1, N + 1)})
-    print(f'#{t} {result}')
+    for idx in range(1, N + 1):
+        find(idx)
+    print(f'#{t} {len(set(parents)) - 1}')
