defdf6f1bc2050ef0bcda903fac20f6b2ffc1aa2
유주성 <aaa4591@naver.com>
2025-10-28 14:11:40 +0900

    폴짝폴짝

--- 알고리즘_스터디/1주차/1326.py
+++ 알고리즘_스터디/1주차/1326.py
@@ -13,14 +13,14 @@ while queue:
         result = now[1]
         print(result)
         break
-    if now[0] + arr[now[0]] <= b:
-        for check in range(now[0] + arr[now[0]], b + 1, arr[now[0]]):
+    if now[0] + arr[now[0]] <= N:
+        for check in range(now[0] + arr[now[0]], N + 1, arr[now[0]]):
             if visited[check] == False:
                 queue.append((check, now[1] + 1))
                 visited[check] = True
 
     if 0 < now[0] - arr[now[0]]:
-        for check in range(now[0] - arr[now[0]], 0, arr[now[0]]):
+        for check in range(now[0] - arr[now[0]], 0, -arr[now[0]]):
             if visited[check] == False:
                 queue.append((check, now[1] + 1))
                 visited[check] = True
