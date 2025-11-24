05505b26f0a3071243a25bfd7c2fb5698b7ff370
Juseong-Yu <aaa4591@naver.com>
2025-11-24 21:26:34 +0900

    dp

--- 알고리즘_스터디/3주차(dp)/9252.py
+++ 알고리즘_스터디/3주차(dp)/9252.py
@@ -1,4 +1,8 @@
 A = list(input())
 B = list(input())
-print(A, B)
-
+dp = [list([0, []]) for _ in range(len(A))  for _ in range(len(B))] # column A, row B
+print(dp)
+for b in range(len(B)):
+    for a in range(len(A)):
+        if A[a] == B[b]:
+            dp[b][a][0] = 
