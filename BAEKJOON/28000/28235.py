50f32f3da8aa5870bab5fdb6bcea273d46f504e3
Juseong-Yu <aaa4591@naver.com>
2026-01-31 10:53:12 +0900

    Easy[

---
+++ BAEKJOON/28000/28235.py
@@ -0,0 +1,9 @@
+call = input()
+if call == 'SONGDO':
+    print('HIGHSCHOOL')
+elif call == "CODE":
+    print('MASTER')
+elif call == "2023":
+    print("0611")
+elif call == "ALGORITHM":
+    print('CONTEST')
