8aa6f77fcd6641d4773f6c4bd68c86891adaf4b8
JuseongYu <juseongyu56@gmail.com>
2026-02-09 16:46:20 +0900

    플로이드 워셜

---
+++ BAEKJOON/11000/11403.py
@@ -0,0 +1,12 @@
+import copy
+N = int(input())
+G = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if G[i][k] == 1 and G[k][j] == 1:
+                G[i][j] = 1
+
+for row in G:
+    print(*row)
