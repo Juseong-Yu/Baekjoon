6934b9dc470236b74c2fabec6709728a97bb5dbd
Juseong-Yu <aaa4591@naver.com>
2026-02-20 20:02:56 +0900

    easy

---
+++ BAEKJOON/30000/30402.py
@@ -0,0 +1,11 @@
+for _ in range(15):
+    line = input().split()
+    for ele in line:
+        if ele == "w":
+            result = 'chunbae'
+        if ele == "b":
+            result = 'nabi'
+        if ele == 'g':
+            result ='yeongcheol'
+
+print(result)
